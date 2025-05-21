4566f7447e14989f3d21b5aae477ea475f567fe9
Nissan Shrestha <nissan.shrestha.a23@icp.edu.np>
2025-05-21 23:54:05 +0545

    3306

--- src/main/java/com/manga/database/DatabaseConnection.java
+++ src/main/java/com/manga/database/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection{
     private final static String databaseName = "book_choda_comic_padha";
     private final static String username = "root";
     private final static String password = "";
-    private final static String jdbcURL = "jdbc:mysql://localhost:4307/" + databaseName;
+    private final static String jdbcURL = "jdbc:mysql://localhost:3306/" + databaseName;
     
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
